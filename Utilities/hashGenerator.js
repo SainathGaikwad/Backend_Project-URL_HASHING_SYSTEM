e7190543369f0c035a22b86a8a1e75da98c3ba98
Abhay Pachauri <146659785+AvviCoder@users.noreply.github.com>
2024-09-05 22:22:23 -0700

    hashGenerator.js

--- Utilities/hashGenerator.js
+++ Utilities/hashGenerator.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-
+// This function generates the random hash for the corresponding url to uniquely identify it.
 const generateHash = () => {
    return crypto.randomBytes(6).toString('hex');
 }
